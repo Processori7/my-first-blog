12dbb8e9cce13c5a85853363c30811f3f222af76
Processori7 <Usenkoigor01@gmail.com>
2023-01-06 17:07:16 +0300

    Добавление даты

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
- 
- 
+from django.utils import timezone
+from django.db import models
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
@@ -8,7 +10,16 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
  
     def __str__(self):
         return self.title
 # Create your models here.
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
