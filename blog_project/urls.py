a1426716c2c3c2890bdb2ac9c65f4f8215ff695c
Processori7 <Usenkoigor01@gmail.com>
2023-01-06 15:31:26 +0300

    My Django site, second commit

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include # новое изменение
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), # новое изменение
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
 
