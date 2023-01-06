819a0286dfa41779e7b8bbeb6aec78410a90852b
Processori7 <Usenkoigor01@gmail.com>
2023-01-06 16:21:34 +0300

    My Django site, second commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")#correct
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/static/"),
+    os.path.join(BASE_DIR, "static"),
 ]
 MEDIA_URL = "/media/"
 
